0ba278f7b3a9ea11a2dc22e0af59d1c371b2b8d5
paulosipov <49653847+paulosipov@users.noreply.github.com>
2025-04-25 10:34:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,24 @@
 import logging
+import os
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from dotenv import load_dotenv
 
-# Настройка логирования
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-logger = logging.getLogger(__name__)
+load_dotenv()
 
-# Функция обработки команды start
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Привет, мир!')
+logging.basicConfig(level=logging.INFO)
 
-def main():
-    # Вставь сюда свой API-токен
-    # Замените 'YOUR_API_TOKEN' на настоящий токен вашего бота
-    updater = Updater("7576203575:AAEp6cuj1K2RqMb1Fp7z0TC86mWxP3R5LjQ", use_context=True)
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет, мир!")
 
-    # Получаем диспатчер для обработки сообщений
-    dp = updater.dispatcher
+def run_bot():
+    token = os.getenv("BOT_TOKEN")
+    if not token:
+        raise Exception("BOT_TOKEN не найден в .env")
 
-    # Обработчик команды /start
-    dp.add_handler(CommandHandler("start", start))
+    app = ApplicationBuilder().token(token).build()
 
-    # Запускаем бота
-    updater.start_polling()
+    app.add_handler(CommandHandler("start", start))
 
-    # Бот будет работать до тех пор, пока не будет прерван
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+    logging.info("Бот запущен...")
+    app.run_polling()
